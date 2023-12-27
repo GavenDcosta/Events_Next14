245b652e9944721198d0dc92afede0174f90497a
Gaven Dcosta <gavendcosta@gmail.com>
2023-12-27 15:29:09 +0530

    search, categories, pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,13 +3,21 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import Collection from '@/components/shared/Collection'
 import { getAllEvents } from '@/lib/actions/event.actions'
+import Search from '@/components/shared/Search'
+import { SearchParamProps } from '@/types'
+import CategoryFilter from '@/components/shared/CategoryFilter'
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+
+  const page = Number(searchParams?.page) || 1
+  const searchText = (searchParams?.query as string) || ""
+  const category = (searchParams?.category as string) || ""
+
 
   const events = await getAllEvents({
-    query: '',
-    category: '',
-    page: 1,
+    query: searchText,
+    category,
+    page,
     limit: 6
   })
 
@@ -42,8 +50,8 @@ export default async function Home() {
         <h2 className='h2-bold'>Trusted By <br/> Thousands of Events</h2>
 
         <div className='flex w-full gap-5 flex-col md:flex-row'>
-           Search
-           CategoryFilter
+           <Search />
+           <CategoryFilter/>
         </div>
 
         <Collection 
@@ -53,7 +61,7 @@ export default async function Home() {
          collectionType = "All_Events"
          limit={6}
          page={1}
-         totalPages={2}
+         totalPages={events?.totalPages}
         />
 
       </section>
